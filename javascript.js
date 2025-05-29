f0699dde97cb2a94775d4cdbba1ab3487b023884
caleb <calebtelfe@gmail.com>
2025-05-29 13:16:49 -0400

    tree insert function

--- javascript.js
+++ javascript.js
@@ -24,4 +24,43 @@ class Tree {
 
         return root;
     }
+
+    insert(insert) {
+        const value = new TreeNode;
+        value.data = insert;
+
+        if(!this.root) {
+            this.root = value;
+            return;
+        }
+
+        let currentRoot = this.root;
+
+        while(currentRoot) {    //runs while evaluting root is not null!
+            if(value.data == currentRoot.data) {break;}; //no duplicates for this tree. im lazy
+
+            if(value.data < currentRoot.data) {  //if our root is less than root we are comparing to
+                 if (currentRoot.left) {
+                    currentRoot = currentRoot.left;
+                 } else {
+                    currentRoot.left = value;
+                    break;
+                 }
+            }
+
+
+
+            if(value.data > currentRoot.data) {  //if our root is more than root we are comparing to
+                if (currentRoot.right) {
+                   currentRoot = currentRoot.right;
+                } else {
+                   currentRoot.right = value;
+                   break;
+                }
+           }
+        }
+
+
+        
+    }
 }
